96256ac68630573364242c690427f2412e14fd70
JuanNew19 <juanma.a0190@gmail.com>
2025-01-21 11:11:52 -0300

    Inicializando proyecto Spring Boot

---
+++ src/test/java/com/new19/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new19.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
